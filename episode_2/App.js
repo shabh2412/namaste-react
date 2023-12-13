6ad27770fc6ecc79e650f5e2add00ecfd0ada20e
Rishabh Panesar <rishabhpanesar@gmail.com>
2023-12-13 06:22:57 +0530

    using parent to render multiple children

--- episode_2/App.js
+++ episode_2/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("customRootForReact"));
 
@@ -12,6 +12,7 @@ const createH1 = ({ id, text }) => {
 };
 
 const h1 = createH1({ id: "heading_1", text: "I am H1" });
-const parent = createDivWithID("parent_div", h1);
+const h1_2 = createH1({ id: "second_h1", text: "I am 2nd H1" });
+const parent = createDivWithID("parent_div", [h1, h1_2]);
 
 root.render(parent);
