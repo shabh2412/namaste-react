ac4247596ecb48195013ffea1030965278abf2f0
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-03-20 03:01:15 +0530

    integrated swagger for better documentation

--- food-ordering-app-be/src/index.js
+++ food-ordering-app-be/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const { restaurant_router } = require("./routers/restaurant.router");
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
+
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +13,8 @@ app.use(express.json());
 
 
 app.use(`/restaurants`, restaurant_router);
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
 const PORT = process.env.PORT;
