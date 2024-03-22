9a373c794d02a31e22560412e7de240758d5984d
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-03-23 01:10:16 +0530

    added login logout button with toggling states

--- food-ordering-app-fe/src/components/Header.js
+++ food-ordering-app-fe/src/components/Header.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import localAssets from "../utils/localAssets";
 
 const Header = () => {
+  const [is_logged_in, set_is_logged_in] = useState(false);
+
+  const toggle_login = () => {
+    set_is_logged_in(prev => !prev);
+  };
+
+  // console.log("rendered");
+
+  // useEffect(() => {
+  //   console.log(is_logged_in, "rendered"); // this will be called once during the first render, and then only when the items in dependency array changes.
+  // }, [is_logged_in]); // this is the dependency array.
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +26,16 @@ const Header = () => {
           <li>Contact Us</li>
           <li>Cart</li>
         </ul>
+        <div style={{ width: "80px", display: "flex", justifyContent: "end", alignItems: "center" }}>
+          <button style={{
+            padding: "8px",
+            borderRadius: "4px"
+          }}
+            onClick={toggle_login}
+          >{
+              is_logged_in ? "Log out" : "Log in"
+            }</button>
+        </div>
       </div>
     </div>
   );
