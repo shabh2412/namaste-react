8ba79a8a4fc7804fe6178b7ae979c288931fb45b
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-04-28 19:54:15 +0530

    using custom hooks

--- food-ordering-app-fe/src/components/RestaurantListings.js
+++ food-ordering-app-fe/src/components/RestaurantListings.js
@@ -1,87 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import useRestaurantListing from "../utils/hooks/useRestaurantListing";
 import RestaurantCard from "./RestaurantCard";
-import { initial_lat_long } from "../utils/mockData";
 import Search from "./Search";
-import { base_url } from "../utils/constants";
-
 // previously: Body & Body.js
 const RestaurantListings = () => {
 
-  const [list_of_restaurants, set_list_of_restaurants] = useState([]);
-  const [filtered_restaurants, set_filtered_restaurants] = useState([]);
-
-  const [minimum_rating, set_minimum_rating] = useState(4);
-
-  const [loading, set_loading] = useState(false);
-
-  const filter_data_based_on_ratings = () => {
-    const filteredList = list_of_restaurants?.filter(item => {
-      return item?.info?.avgRating >= minimum_rating;
-    });
-    set_filtered_restaurants(filteredList);
-  };
-
-  const fetch_data = async ({ lat, long } = initial_lat_long) => {
-    try {
-      set_loading(true);
-      const url = `${base_url}/list?lat=${lat}&long=${long}`;
-      const res = await fetch(url);
-      const json_data = await res?.json();
-      console.log(json_data);
-      const element = json_data?.data?.cards?.find((item) => item?.card?.card?.id === "restaurant_grid_listing");
-      console.log({ element });
-      set_list_of_restaurants(
-        element?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
-        json_data?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants || json_data?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
-      );
-      set_filtered_restaurants(
-        element?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
-        json_data?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants || json_data?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
-      );
-    } catch (error) {
-      console.log({ error });
-    } finally {
-      set_loading(false);
-    }
-  };
-
-  useEffect(() => {
-    filter_data_based_on_ratings();
-  }, [list_of_restaurants]);
-
-  const [current_lat_long, set_current_lat_long] = useState(initial_lat_long);
-
-  useEffect(() => {
-    if (!navigator?.geolocation) {
-      console.log("geolocation is not supported.");
-    } else {
-      set_loading(true);
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { coords: { latitude, longitude } } = position;
-        set_current_lat_long({
-          lat: latitude,
-          long: longitude,
-        });
-        fetch_data({ lat: latitude, long: longitude });
-      }, () => {
-        fetch_data();
-      }, {
-        enableHighAccuracy: false,
-        timeout: 5000,
-        maximumAge: Infinity
-      }
-      );
-    }
-  }, []);
-
-  const timeout = useRef(null);
-
-  const debounce = (func, delay) => {
-    if (timeout?.current) {
-      clearTimeout(timeout?.current);
-    }
-    timeout.current = setTimeout(func, delay);
-  };
+  const { current_lat_long, filtered_restaurants, list_of_restaurants, set_filtered_restaurants, minimum_rating, set_minimum_rating, filter_data_based_on_ratings, loading } = useRestaurantListing();
 
   return (
     <div className="body">
--- food-ordering-app-fe/src/components/RestaurantMenu.js
+++ food-ordering-app-fe/src/components/RestaurantMenu.js
@@ -4,19 +4,13 @@ import { base_url } from "../utils/constants";
 import { sample_restaurant_meta } from "../utils/mockData";
 import MenuDropDown from "./MenuDropDown";
 import RestaurantMeta from "./RestaurantMeta";
+import useRestaurantMenuData from "../utils/hooks/useRestaurantMenuData";
+import NotFoundPage from "./NotFound";
 const RestaurantMenu = () => {
 
   const { restaurant_id } = useParams();
 
-  const [search_params,] = useSearchParams();
-
-  const [menu_loading, set_menu_loading] = useState(false);
-
-  const [error, set_error] = useState(false);
-
-  const [restaurant_meta, set_restaurant_meta] = useState({});
-
-  const [restaurant_menu_main, set_restaurant_menu_main] = useState([]);
+  const { error, menu_loading, restaurant_menu_main, restaurant_meta } = useRestaurantMenuData({ restaurant_id });
 
   const [restaurant_menu_copy, set_restaurant_menu_copy] = useState([]);
 
@@ -26,44 +20,6 @@ const RestaurantMenu = () => {
     const { checked, name } = e?.target;
     set_veg_nonveg_filter(prev => ({ ...prev, [name]: checked }));
   };
-
-  const get_restaurant_menu = async () => {
-    try {
-      set_menu_loading(true);
-      const lat = search_params?.get("lat");
-      const long = search_params?.get("long");
-
-      const query_params = new URLSearchParams({
-        lat, long, restaurantId: restaurant_id
-      });
-
-      const response = await fetch(`${base_url}/menu?${query_params}`);
-      const result = await response?.json();
-      // console.log(result);
-      if (result?.data && result?.data?.cards && result?.data?.cards?.length > 0) {
-        const { cards } = result?.data;
-        const res_meta_card_obj = cards?.find(cardObj => cardObj?.card?.card?.info);
-        if (res_meta_card_obj) {
-          const { card: { card: { info: res_meta_card } } } = res_meta_card_obj || {};
-          set_restaurant_meta(res_meta_card);
-        }
-        const res_menu_cards_obj = cards?.find(cardObj => cardObj?.groupedCard?.cardGroupMap?.REGULAR?.cards);
-        if (res_menu_cards_obj) {
-          const { groupedCard: { cardGroupMap: { REGULAR: { cards: res_menu_cards } } } } = res_menu_cards_obj;
-          set_restaurant_menu_main(res_menu_cards?.filter(item => item?.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"));
-        }
-      }
-    } catch (error) {
-      set_error(true);
-      console.log(`error occured while loading menu`, error);
-    } finally {
-      set_menu_loading(false);
-    }
-  };
-
-  useEffect(() => {
-    get_restaurant_menu();
-  }, []);
 
   const onlyVegFilter = () => {
     if (veg_nonveg_filter?.veg) {
@@ -103,33 +59,41 @@ const RestaurantMenu = () => {
   }, [restaurant_menu_main]);
 
   return <>
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-    }}>
-      <div style={{
-        maxWidth: "800px",
-        minHeight: "600px",
-        margin: "20px auto 0",
-        width: "100%"
-      }}>
-        <RestaurantMeta restaurant_meta={restaurant_meta} />
-        {/* Veg/NonVeg Filter */}
-        <div className="flex gap-1 items-center justify-start my-1 mx-1" >
-          <div className="flex" >
-            <div>Veg Only</div>
-            <input name="veg" type="checkbox" checked={veg_nonveg_filter?.veg} onChange={toggle_veg_nonveg} />
-          </div>
-          {/* <div className="flex" >
+    {
+      error ? <NotFoundPage /> :
+        (
+          menu_loading ? <>
+            Loading...
+          </> :
+            <div style={{
+              display: "flex",
+              flexDirection: "column",
+            }}>
+              <div style={{
+                maxWidth: "800px",
+                minHeight: "600px",
+                margin: "20px auto 0",
+                width: "100%"
+              }}>
+                <RestaurantMeta restaurant_meta={restaurant_meta} />
+                {/* Veg/NonVeg Filter */}
+                <div className="flex gap-1 items-center justify-start my-1 mx-1" >
+                  <div className="flex" >
+                    <div>Veg Only</div>
+                    <input name="veg" type="checkbox" checked={veg_nonveg_filter?.veg} onChange={toggle_veg_nonveg} />
+                  </div>
+                  {/* <div className="flex" >
             <div>Non Veg</div>
             <input type="checkbox" checked={veg_nonveg_filter?.nonveg} />
           </div> */}
-        </div>
-        {
-          restaurant_menu_copy?.map(menu => <MenuDropDown key={`${menu?.card?.card?.title}`} title={menu?.card?.card?.title} itemCards={menu?.card?.card?.itemCards} />)
-        }
-      </div>
-    </div>
+                </div>
+                {
+                  restaurant_menu_copy?.map(menu => <MenuDropDown key={`${menu?.card?.card?.title}`} title={menu?.card?.card?.title} itemCards={menu?.card?.card?.itemCards} />)
+                }
+              </div>
+            </div>
+        )
+    }
   </>;
 };
 
