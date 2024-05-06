1961794c1aed00a14f5755dbca28b1209b7d5512
Rishabh Panesar <rishabhpanesar@gmail.com>
2024-05-06 11:29:31 +0530

    separated episode 10 code

---
+++ episode_10/index.js
@@ -0,0 +1,55 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { createRoot } from "react-dom/client";
+import App from "./src/App";
+// import About from "./src/components/About";
+import ContactPage from "./src/components/ContactPage";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NotFoundPage from "./src/components/NotFound";
+// import RestaurantListings from "./src/components/RestaurantListings";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const rootContainer = document.getElementById("root");
+const root = createRoot(rootContainer);
+
+const RestaurantListings = lazy(() => import("./src/components/RestaurantListings"));
+const About = lazy(() => import("./src/components/About"));
+
+const routerConfiguration = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <Suspense fallback={<h1>Loading...</h1>} >
+          <RestaurantListings />,
+        </Suspense>
+      },
+      {
+        path: "/about",
+        element: <Suspense fallback={<h1>Loading...</h1>}>
+          <About />,
+        </Suspense>
+      },
+      {
+        path: "/contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "/restaurants/:restaurant_id",
+        element: <RestaurantMenu />
+      },
+    ]
+  },
+]);
+
+root.render(
+  <RouterProvider router={routerConfiguration} />
+);
